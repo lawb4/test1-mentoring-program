0a77d53f7dab6a436c156726a4664c3386f992d2
Yahor Labanau <yahorlabanau@gmail.com>
2021-10-16 16:14:06 +0300

    module3 - cycles
    solved the 'cycles.js' task

--- tasks/cycles/cycles.js
+++ tasks/cycles/cycles.js
@@ -8,26 +8,57 @@
 let forFactorial, whileFactorial, doFactorial;
 const n = 10; //10! = 3628800
 
+forFactorial = whileFactorial = doFactorial = 1;
+let i = n;
+
+for (i = n; i > 0; i--) {
+  forFactorial *= i;
+}
+
+i = n;
+while (i > 0) {
+  whileFactorial *= i;
+  i--;
+}
+
+i = n;
+do {
+  doFactorial *= i;
+  i--;
+} while (i > 0);
+
 /** TODO
  * return concatenated string from an array of substring
  * assign result to variable str
  */
-let str;
+let str = '';
 const substr = ["I", " love", " JS"];
 
+for (let i = 0; i < substr.length; i++) {
+  str += substr[i];
+}
 
+// Using 'for..of'
+// for (let word of substr) {
+//   str += word;
+// }
 
 /** TODO
  * calculate a total of income of certain person
  * assign result to variable totalIncome
  */
-let totalIncome;
+let totalIncome = 0;
 const personIncomes = {
   salary: 1985,
   rent: -600,
   interestOnDeposit: 250,
   otherExpences: -300
 };
+
+for (const el in personIncomes) {
+  totalIncome += personIncomes[el];
+}
+console.log(totalIncome);
 
 
 module.exports = {
