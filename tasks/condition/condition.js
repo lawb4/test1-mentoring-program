fce4cf2b3eed6b5aef9db0544d468e6d489a1922
Yahor Labanau <yahorlabanau@gmail.com>
2021-10-26 08:12:57 +0300

    Module3 (#5)
    * solved module3
    
    solved task1,2,3 from module3
    
    * module3
    
    fixed style error in ternary operator
    
    * module3 - cycles
    
    solved the 'cycles.js' task
    
    * module3 - updated 'condition.js'
    
    refactored 'if statements' to 'switch'
    
    * module3 - cycles.js - v1.1
    
    * module3-after-meeting-changes

--- tasks/condition/condition.js
+++ tasks/condition/condition.js
@@ -12,8 +12,13 @@ const x = 3;
 const y = 7;
 
 //TODO put your code here
+if (y < 10) {
+    z1 = x + y;
+} else {
+    z1 = x * y;
+}
 
-
+z2 = (y < 10) ? (x + y) : (x * y);
 
 /** TODO
  * The system receives 3 params - alpha, beta, operator.
@@ -27,7 +32,20 @@ const beta = 7;
 const operator = "add";
 
 //TODO put your code here
-
+switch (operator) {
+    case 'add':
+        task2 = alpha + beta;
+        break;
+    case 'subtract':
+        task2 = alpha - beta;
+        break;
+    case 'multiply':
+        task2 = alpha * beta;
+        break;
+    case 'divide':
+        task2 = alpha / beta;
+        break;
+}
 
 
 /**
@@ -45,7 +63,15 @@ const redemption = 420;
 let discount;
 
 //TODO put your code here
-
+if (redemption <= 350) {
+    discount = 0;
+} else if (redemption <= 1350) {
+    discount = 15;
+} else if (redemption <= 2700) {
+    discount = 30;
+} else if (redemption <= 6500) {
+    discount = 45;
+}
 
 module.exports = {
     z1,
--- tasks/cycles/cycles.js
+++ tasks/cycles/cycles.js
@@ -8,26 +8,57 @@
 let forFactorial, whileFactorial, doFactorial;
 const n = 10; //10! = 3628800
 
+forFactorial = whileFactorial = doFactorial = 1;
+
+for (let i = 1; i <= n; i++) {
+  forFactorial *= i;
+}
+
+{
+  let i = 1;
+  while (i <= n) {
+    whileFactorial *= i++;
+  }
+}
+
+{
+  let i = 1;
+  do {
+    doFactorial *= i++;
+  } while (i <= n);
+}
+
 /** TODO
  * return concatenated string from an array of substring
  * assign result to variable str
  */
-let str;
+let str = '';
 const substr = ["I", " love", " JS"];
 
+for (let i = 0; i < substr.length; i++) {
+  str += substr[i];
+}
 
+// Using 'for..of'
+// for (let word of substr) {
+//   str += word;
+// }
 
 /** TODO
  * calculate a total of income of certain person
  * assign result to variable totalIncome
  */
-let totalIncome;
+let totalIncome = 0;
 const personIncomes = {
   salary: 1985,
   rent: -600,
   interestOnDeposit: 250,
   otherExpences: -300
 };
+
+for (const el in personIncomes) {
+  totalIncome += personIncomes[el];
+}
 
 
 module.exports = {
