ae15c53f54719277710b429e59d26c57f50f708a
Yahor Labanau <yahorlabanau@gmail.com>
2021-10-26 08:00:23 +0300

    module6 - v1

--- tasks/classes/classes.js
+++ tasks/classes/classes.js
@@ -4,7 +4,15 @@
  * 2) constructor that pass firstName and lastName to the object
  * 3) method getFullName
  */
-//put your code here
+class Person {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    getFullName() {
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
 
 /**
  * implement class Student that extends Person with:
@@ -12,10 +20,18 @@
  * 2) constructor that pass grade to the object
  * 3) method getGrade that returns property grade
  */
-//put your code here
+class Student extends Person {
+    constructor(grade) {
+        super();
+        this.grade = grade;
+    }
+    getGrade() {
+        return this.grade;
+    }
+}
 
-const person = ''; //create instance of class person
-const student = ''; //create instance of class student
+const person = new Person(); //create instance of class person
+const student = new Student(); //create instance of class student
 
 module.exports = {
     person,
--- tasks/classes/pageObject.js
+++ tasks/classes/pageObject.js
@@ -3,18 +3,91 @@
  */
 
 class BasePage {
-	
+    constructor(baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    get header() {
+        return new Header('Header');
+    }
+
+    get footer() {
+        return new Footer('Footer');
+    }
+
+    open(url) {
+        return `${this.baseUrl}/${url}`;
+    }
 }
 
-class LoginPage {
+class LoginPage extends BasePage {
+    constructor(baseUrl, header, footer) {
+        super(baseUrl, header, footer);
+        this.email = this.email_field;
+        this.password = this.password_field;
+        this.submitButton = this.submit_button;
+    }
 
+    get email_field() {
+        return 'email field';
+    }
+
+    get password_field() {
+        return 'password field';
+    }
+
+    get submit_button() {
+        return 'submit button';
+    }
+
+    open() {
+        return super.open('/loginUrl');
+    }
+
+    typeEmail(email) {
+        return `Email ${email} has been typed into the ${this.email_field}.`;
+    }
+
+    typePassword(password) {
+        return `Password ${password} has been typed into the ${this.password_field}.`;
+    }
+
+    clickSubmit() {
+        return `The button ${this.submit_button} has been clicked.`;
+    }
 }
 
 class Component {
-	
+    constructor(type) {
+        this.type = type;
+    }
+
+    review() {
+        return `${this.type} -> reviewed`;
+    }
 }
 
+class Header extends Component {
+    constructor(name) {
+        super(name);
+    }
+}
+
+class Footer extends Component {
+    constructor(name) {
+        super(name);
+    }
+
+    review() {
+        return `${super.review()}`;
+    }
+}
+// const myFooter = new Footer("footer");
+// console.log(myFooter.review());
+
+//const loginPage = new LoginPage();
+
 module.exports = {
-	BasePage,
-	LoginPage
+    BasePage,
+    LoginPage
 };
